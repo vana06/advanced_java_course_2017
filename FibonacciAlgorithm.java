774cf128f5836f53b71edc3650e460ffe9bd4ab3
VladlenKaraush <vladlenkaraush@gmail.com>
2017-11-08 20:14:56 +0300

    Delete FibonacciAlgorithm.java

--- FibonacciAlgorithm.java
+++
@@ -1,7 +0,0 @@
-public interface FibonacciAlgorithm {
-    /**
-     * Получение числа фибоначчи в десятичном виде по индексу. Например, F(1)=1, F(2)=1, F(3)=2, F(5)=5 и т.д.
-     *
-     */
-    String evaluate(int index);
-}
